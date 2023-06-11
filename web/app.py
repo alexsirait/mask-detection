f332df7389e1fcbcfa2a344472cac00880eb77f3
Alekk <106827387+alexsirait@users.noreply.github.com>
2023-06-11 15:42:14 +0700

    first commit

---
+++ web/app.py
@@ -0,0 +1,78 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'maskdetection.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = load_img(img_path, target_size=(400, 400))
+
+    # Preprocessing the image
+    x = img_to_array(img)
+    # x = np.true_divide(x, 255)
+    # x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    # x = preprocess_input(x, mode='caffe')
+    x = x/255
+    proba = model.predict(x.reshape(1, 400, 400, 3))
+    # b=res[np.argmax(proba)]
+    return proba
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        res = ["Tidak Menggunakan Masker !", "Masker Terdeteksi"]
+
+        b = res[np.argmax(preds)]
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        # result = str(pred_class[0][0][1])               # Convert to string
+        os.remove('./uploads/' + f.filename)
+        return b
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=False)
